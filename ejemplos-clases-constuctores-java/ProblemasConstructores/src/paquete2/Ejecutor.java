007447e30fae7a9f1f6bf73ba146faa897318595
bitxanax <bitxanax@gmail.com>
2021-04-28 17:43:44 -0500

    Seccond commit

--- ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete2/Ejecutor.java
+++ ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete2/Ejecutor.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package paquete2;
-
+import java.util.Scanner;
 /**
  *
  * @author reroes
@@ -15,8 +15,21 @@ public class Ejecutor {
         /*Crear un objeto y presentar los datos
         Ingresar la información por teclado.
         */
-        
-	Hospital hospital = new Hospital("Loja", "Agustin", 20, 20);
+        Scanner read = new Scanner(System.in);
+	
+	System.out.println("Ingrese nombre ciudad: ");
+	String nombreCiudad = read.next()
+	
+	System.out.println("Ingrese nombre hospital: ");
+	String nombreHospital = read.next();
+	
+	System.out.println("Ingrese número doctores: ");
+	int numDoctores = read.nextInt();
+
+	System.out.println("Ingrese número enfermeros");
+	int numEnfermeros = read.nextInt();
+	
+	Hospital hospital = new Hospital(nombreCiudad, nombreHospital, numDoctores, numEnfermeros);
 	System.out.println(hospital.toString());        
-    }
+   }
 }
